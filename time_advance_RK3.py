09380536d22ba3884be2c43b6a77c449b3e221ef
M.L. Garcia <garcia.mlawrence@gmail.com>
2023-05-23 01:24:57 -0700

    Updated RK3 solver to use numpy more

--- time_advance_RK3.py
+++ time_advance_RK3.py
@@ -15,20 +15,25 @@ def time_advance_RK3(grid):
     # Maximum timestep for numerical stability (CFL condition)
     h = abs(min(C*grid.dx/np.max(grid.u), C*grid.dy/np.max(grid.v)))
 
+    # initial conditions
     u0, v0, w0 = grid.u, grid.v, grid.w
     u,v,w = u0,v0,w0
 
-    Fu, Fv, Fw = np.zeros(3)
-    for i in range(3):
-        grid.u = u0 + h*np.dot(a[i:(i+1)])
-        grid.v = v0 + h*np.dot(a[i:(i+1)])
-        grid.w = w0 + h*np.dot(a[i:(i+1)])
+    # Have d/dx_i F each be a numpy array
+    Fu, Fv, Fw = np.zeros([grid.Nx,grid.Ny,grid.Nz,3])
 
-        [Fu[i], Fv[i], Fw[i]] = compute_RHS(grid)
-        [grid.u, grid.v, grid.w] = compute_projection_step(Fu[i], Fv[i], Fw[i])
-    
-    grid.u = u0 + h * np.dot(b,Fu)
-    grid.u = v0 + h * np.dot(b,Fv)
-    grid.u = w0 + h * np.dot(b,Fw)
+    for i in range(3):
+        grid.u = u0 + h*np.sum(Fu * a[i:(i+1)], axis=-1)
+        grid.v = v0 + h*np.sum(Fv * a[i:(i+1)], axis=-1)
+        grid.w = w0 + h*np.sum(Fw * a[i:(i+1)], axis=-1)
+
+        grid.u, grid.v, grid.w = compute_projection_step(grid.u, grid.v, grid.w)
+        Fui, Fvi, Fwi = compute_RHS(grid)
+        
+        Fu[..., i], Fv[..., i], Fw[..., i] = compute_projection_step(Fui, Fvi, Fwi)
+
+    grid.u = u0 + h * np.sum(Fu * b, axis=-1)
+    grid.v = v0 + h * np.sum(Fv * b, axis=-1)
+    grid.w = w0 + h * np.sum(Fw * b, axis=-1)
 
     return grid
