bc27a9e7942ede349a8812cca347e95c9f689382
M.L. Garcia <garcia.mlawrence@gmail.com>
2023-05-23 15:53:42 -0700

    Initialized grid and Taylor-Green vortex  function

--- grid.py
+++ grid.py
@@ -4,18 +4,41 @@ import numpy as np
 class grid:
     
     # initialize grid of ones
-    def fill(self, Nx=64,
-                   Ny=64,
-                   Nz=64,
-                   dx=32/np.pi,
-                   dy=32/np.pi,
-                   dz=32/np.pi):
-        pass
+    def __init__(self, Nx, Ny, Nz, dx, dy, dz):
+        self.Nx = Nx
+        self.Ny = Ny
+        self.Nz = Nz
+        self.dx = dx
+        self.dy = dy
+        self.dz = dz
+
+        # coordinates defined on the center
+        # Indexed as u[X][Y][Z]
+        x = np.reshape(np.linspace(dx/2,Nx*dx,Nx), (Nx, 1, 1))
+        y = np.reshape(np.linspace(dy/2,Ny*dy,Ny), (1, Ny, 1))
+        z = np.reshape(np.linspace(dz/2,Nz*dz,Nz), (1, 1, Nz))
+
+        # duplicating row vectors to make a numpy array
+        self.x = np.tile(x, (1,Ny,Nz))
+        self.y = np.tile(y, (Nx,1,Nz))
+        self.z = np.tile(z, (Nx,Ny,1))
+
+        # initializing velocity, pressure fields with ones
+        self.u = np.ones([Nx,Ny,Nz])
+        self.v = np.ones([Nx,Ny,Nz])
+        self.w = np.ones([Nx,Ny,Nz])
+        self.p = np.ones([Nx,Ny,Nz])
 
     # read from file
     def read(self,filename):
         pass
 
     # Taylor-Green Vortex
-    def vortex(self):
-        pass
+    def vortex(self,A,B,C,a,b,c):
+        if (A*a + B*b + C*c != 0):
+            raise ValueError('Not well defined. A*a + B*b + C*C != 0.')
+        
+        # place u,v,w nodes in staggered grid
+        self.u = A * np.cos(a*(self.x - self.dx)) * np.sin(b*self.y) * np.sin(c*self.z)
+        self.v = B * np.sin(a*self.x) * np.cos(b*(self.y - self.dy)) * np.sin(c*self.z)
+        self.w = C * np.sin(a*self.x) * np.sin(b*self.y) * np.cos(c*(self.z - self.dz))
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-global nu,dx,dy,dz,dt,kxx,kyy,kzz,Nx,Ny,Nz
+from grid import *
+
+global nu,dx,dy,dz,dt,kxx,kyy,kzz,Nx,Ny,Nz,Re
+
+# TODO: Replace with user input
+Lx = 1
+Ly = 1
+Lz = 1
+Nx = 64
+Ny = 64
+Nz = 64
+dx = np.pi * 2 / Nx
+dy = np.pi * 2 / Ny
+dz = np.pi * 2 / Nz
+time = 0
+U0 = 1
+Re = U0 * Lx / 1e-3
+
+# initializing grid
+grid = grid(Nx,Ny,Nz,dx,dy,dz)
