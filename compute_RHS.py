Fixed compute_projection step  wavenumber calculation

compute_RHS.py
@@ -18,7 +18,7 @@ def compute_RHS(grid):
     # d(uw)/dz
     uw = 0.5*(grid.u + np.roll(grid.u,1,axis=2)) * \
          0.5*(grid.w + np.roll(grid.w,1,axis=0))
-    Fu -= (np.roll(uw,-1,axis=1) - uw) / grid.dz
+    Fu -= (np.roll(uw,-1,axis=2) - uw) / grid.dz
 
     # 1/Re * d^2(u)/d(x)^2
     Fu += grid.nu *(np.roll(grid.u,-1,axis=0) - 2*grid.u + np.roll(grid.u,1,axis=0)) / grid.dx**2
@@ -33,16 +33,16 @@ def compute_RHS(grid):
     # d(uv)/dx
     uv = 0.5*(grid.v + np.roll(grid.v,1,axis=0)) * \
          0.5*(grid.u + np.roll(grid.u,1,axis=1))
-    Fv -= (np.roll(uv,-1,axis=1) - uv) / grid.dx
+    Fv -= (np.roll(uv,-1,axis=0) - uv) / grid.dx
 
     # d(vv)/dy
     vv = (0.5*(grid.v + np.roll(grid.v,-1,axis=1)))**2
-    Fv -= (vv - np.roll(vv,1,axis=0)) / grid.dy
+    Fv -= (vv - np.roll(vv,1,axis=1)) / grid.dy
 
     # d(vw)/dz
     vw = 0.5*(grid.v + np.roll(grid.v,1,axis=2)) * \
          0.5*(grid.w + np.roll(grid.w,1,axis=1))
-    Fv -= (np.roll(vw,-1,axis=1) - vw) / grid.dz
+    Fv -= (np.roll(vw,-1,axis=2) - vw) / grid.dz
 
     # 1/Re * d^2(v)/d(x)^2
     Fv += grid.nu *(np.roll(grid.v,-1,axis=0) - 2*grid.v + np.roll(grid.v,1,axis=0)) / grid.dx**2
@@ -57,7 +57,7 @@ def compute_RHS(grid):
     # d(uw)/dx
     uw = 0.5*(grid.w + np.roll(grid.w,1,axis=0)) * \
          0.5*(grid.u + np.roll(grid.u,1,axis=2))
-    Fw -= (np.roll(uw,-1,axis=1) - uw) / grid.dx
+    Fw -= (np.roll(uw,-1,axis=0) - uw) / grid.dx
 
     # d(uw)/dx
     vw = 0.5*(grid.w + np.roll(grid.w,1,axis=1)) * \
@@ -65,7 +65,7 @@ def compute_RHS(grid):
     Fw -= (np.roll(vw,-1,axis=1) - vw) / grid.dy
 
     # d(ww)/dz
-    ww = (0.5*(grid.w + np.roll(grid.w,-1,axis=1)))**2
+    ww = (0.5*(grid.w + np.roll(grid.w,-1,axis=2)))**2
     Fw -= (ww - np.roll(ww,1,axis=2)) / grid.dz
 
     # 1/Re * d^2(w)/d(x)^2
@@ -74,5 +74,6 @@ def compute_RHS(grid):
     Fv += grid.nu *(np.roll(grid.w,-1,axis=1) - 2*grid.w + np.roll(grid.w,1,axis=1)) / grid.dy**2
     # 1/Re * d^2(w)/d(z)^2
     Fv += grid.nu *(np.roll(grid.w,-1,axis=2) - 2*grid.w + np.roll(grid.w,1,axis=2)) / grid.dz**2
+
 
     return Fu, Fv, Fw

compute_projection_step.py
@@ -19,7 +19,7 @@ def compute_projection_step(grid, principal=True): # if True, use u,v,p - if Fal
     for index, value in np.ndenumerate(grid.p):
         kk = grid.kxx[index[0]] + grid.kyy[index[1]] + grid.kzz[index[2]]
         if (not np.array_equal(index, [0,0,0])):
-            phat[index] /= kk    
+            phat[index] /= kk
     
     grid.p = np.fft.ifftn(phat).real
 

grid.py
@@ -49,11 +49,11 @@ class grid:
                           2*(np.cos(2*np.pi*(-self.Ny+j)/self.Ny)-1)/self.dy**2
 
         for k in range(self.Nz):
-            self.kzz[i] = 2*(np.cos(2*np.pi*i/self.Nz)-1)/self.dz**2 if i > self.Nz / 2 else \
-                          2*(np.cos(2*np.pi*(-self.Nz+i)/self.Nz)-1)/self.dz**2
+            self.kzz[k] = 2*(np.cos(2*np.pi*k/self.Nz)-1)/self.dz**2 if k < self.Nz / 2 else \
+                          2*(np.cos(2*np.pi*(-self.Nz+k)/self.Nz)-1)/self.dz**2
 
     # Taylor-Green Vortex
-    def vortex(self,A,B,C,a,b,c):
+    def vortex(self,A,a,B,b,C,c):
         if (A*a + B*b + C*c != 0):
             raise ValueError('Not well defined. A*a + B*b + C*C != 0.')
 

main.py
@@ -32,11 +32,7 @@ rsdlsu = np.array([0])  # residual values of u
 rsdlsv = np.array([0])  # residual values of v
 rsdlsv = np.array([0])  # residual values of w
 
-while (time < 3):
-    
-    if (i % 100 == 0):
-        plt.imshow(grid_DNS.u[...,8], interpolation='nearest')
-        plt.show()
+while (time < 1.5):
 
     grid_DNS, h = time_advance_RK3(grid_DNS)
     
@@ -59,13 +55,19 @@ while (time < 3):
             print("URSD: " + str(abs(uvals[i] - uvals[i-1])))
             print("VRSD: " + str(abs(vvals[i] - vvals[i-1])))
             print("==========================================")
+
+            if (i % 10 == 0):
+                plt.imshow(grid_DNS.u[...,8], interpolation='nearest')
+                plt.show()
     
-    # check for divergence-free velocity field    
+    # check for divergence-free velocity field
+    """
     if (np.max((np.roll(grid_DNS.u,-1,axis=0) - grid_DNS.u) / grid_DNS.dx + \
                 (np.roll(grid_DNS.v,-1,axis=1) - grid_DNS.v) / grid_DNS.dy + \
                 (np.roll(grid_DNS.w,-1,axis=2) - grid_DNS.w) / grid_DNS.dz) > 1e+10):
         raise ValueError('Velocity field is not divergence free')
-        
+    """
+
     i += 1
     time += h
 

time_advance_RK3.py
@@ -33,7 +33,7 @@ def time_advance_RK3(grid):
         grid.Fu, grid.Fv, grid.Fw = compute_RHS(grid)
 
         # remove divergence from Fu, Fv, Fw
-        Fu[..., i], Fv[..., i], Fw[..., i] = compute_projection_step(grid,False)
+        Fu[:,:,:,i], Fv[:,:,:,i], Fw[:,:,:,i] = compute_projection_step(grid,False)
 
     grid.u = u0 + h * np.sum(Fu * b, axis=-1)
     grid.v = v0 + h * np.sum(Fv * b, axis=-1)