Corrected closure calculation

training/compute_closure.py
@@ -27,13 +27,24 @@ def dtau_del(tau, delta, index, grid_spacing):
         return torch.tensor(dtau, dtype=torch.float32)
 
 # reform dtau_del as a torch function
+# to eliminate compute_tau.py, we instead compute the closure term directly from the eddy viscosity
 class nu_deriv_funct(torch.autograd.Function):
-        def forward(ctx, input_data, tau, delta, index, grid_spacing):
-                pred_tau = cp.deepcopy(tau)
-                pred_tau[index] = input_data.detach().numpy()
+        @staticmethod
+        def forward(ctx, input_data, S, delta, index, grid_spacing, tau_act):
+                tau = -2 * input_data * S[index[0], index[1], index[2]]
+                ctx.save_for_backward(torch.tensor(index, dtype=torch.int), torch.tensor(grid_spacing, dtype=torch.float32), torch.tensor(S, dtype=torch.float32))
+                pred_tau = cp.deepcopy(tau_act)
+                pred_tau[index[0], index[1], index[2]] = tau
                 return dtau_del(pred_tau, delta, index, grid_spacing)
         
+        @staticmethod
         def backward(ctx, grad_output):
                 # d closure_k / d tau_ij
-                grad = torch.tile(grad_output/3, (3,1))
-                return grad, None, None, None, None
+                index = ctx.saved_tensors[0].detach().numpy()
+                grid_spacing = ctx.saved_tensors[1].detach().numpy()
+                S = ctx.saved_tensors[2]
+                delta = torch.zeros([3,1])
+                dnudclose = -2 * dtau_del(S, delta, index, grid_spacing)
+                grad = grad_output * dnudclose
+                grad = grad.sum().view(1)
+                return -1*grad, None, None, None, None, None